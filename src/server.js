0414c1aed27f495895ec0d617aef155384f9d465
Paulo Luan <pauloluan1409@gmail.com>
2024-08-02 18:52:11 -0300

    Insomnia

--- src/server.js
+++ src/server.js
@@ -12,17 +12,79 @@
 // UserController.create(request);
 // UserController.list();
 
-const ProductController = require('./controllers/ProductController');
+// const ProductController = require('./controllers/ProductController');
 
-const request = {
-    body:{
-        name: "Blusa2",
-        description: "Blusa Preta",
-        price: 49.99,
-        enabled: 1,
-        stock: 5
-    }
-};
+// const request = {
+//     body:{
+//         name: "Blusa2",
+//         description: "Blusa Preta",
+//         price: 49.99,
+//         enabled: 1,
+//         stock: 5
+//     }
+// };
 
-// ProductController.create(request);
-ProductController.list();
+// // ProductController.create(request);
+// ProductController.list();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+
+
+
+
+const ProductModel = require('./models/ProductModel')
+const UserModel = require('./models/UserModel')
+
+app.get('/', function(request, response){
+    response.send("Minha primeira rota 1 backend");
+});
+
+app.get('/produtos', async function(request, response){
+    const products = await ProductModel.findAll();
+    response.json(products);
+});
+
+app.post('/produtos', async function(request, response){
+    ProductModel.create(request.body);
+    return response.json({
+        message: "Produto cadastrado com sucesso"
+    })
+})
+
+app.delete('/produtos/:id', async function(request, response){
+    ProductModel.destroy({
+        where:{
+            id: request.params.id
+        }
+    })
+
+    return response.json({
+        message: "Produto deletado com sucesso"
+     })
+  });
+
+app.get('/users', async function(request, response){
+    const users = await UserModel.findAll();
+    response.json(users);
+});
+
+app.post('/users/', async function(request, response){
+    // console.log("Body", request.body);
+    // console.log("Query", request.query);
+    // console.log("Params", request.params);
+    UserModel.create(request.body);
+    return response.json({
+        message: "Usuario criado com sucesso"
+    })
+
+    // UserModel.create()
+});
+
+
+
+
+app.listen(3000);
